39ad973dd3b0e5fe00e57266ad14898c2b39948c
= <huseyintkt41@gmail.com>
2020-11-24 22:38:18 +0100

    cinema room manager completed stage-1

---
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -0,0 +1,23 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        System.out.print("Cinema:\n");
+        for(int i=0; i<8; i++){
+            if(i==0){
+                System.out.print("  ");
+                for (int j=0; j<8; j++){
+                    System.out.print(j+1 + " ");
+                }
+                System.out.print("\n");
+            }else{
+                System.out.print(i + " ");
+                for(int k=0; k<8; k++){
+                    System.out.print("S ");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
